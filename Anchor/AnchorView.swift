1cb5d89f842d5f4bf3e1ee60a05c3dd5c7c591ba
Armando Castaneda Elguero <armando.cas27@gmail.com>
2019-03-30 09:57:40 -0500

    constructor for AnchorView, adds gesture recognizer for outside tap, and pan recognizer

--- Anchor/AnchorView.swift
+++ Anchor/AnchorView.swift
@@ -7,3 +7,29 @@
 //
 
 import UIKit
+public class AnchorView: UIView {
+    public var parentView: UIView?
+    public var contentView = UIScrollView()
+    public init(contentView: UIScrollView, parentView: UIView? = nil) {
+        super.init(frame: .zero)
+        
+        self.contentView = contentView
+        self.parentView = parentView ?? UIApplication.shared.keyWindow
+        
+        let outsideTap = UITapGestureRecognizer(target: self, action: #selector(handleOutsideTap))
+        addGestureRecognizer(outsideTap)
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
+        contentView.addGestureRecognizer(pan)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+extension AnchorView {
+    @objc func handleOutsideTap() {
+extension AnchorView {
+    @objc func handlePan(recognizer: UIPanGestureRecognizer) {
+    }
+}
