beae84360e6671120547bcbe71e6a4931e68439c
Armando Castaneda Elguero <armando.cas27@gmail.com>
2019-03-30 10:25:07 -0500

    animateTo is responsible for animating the View to a specific anchor position

--- Anchor/AnchorView.swift
+++ Anchor/AnchorView.swift
@@ -11,6 +11,9 @@ public class AnchorView: UIView {
     lazy var heightContraint = NSLayoutConstraint(item: contentView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 0.0)
     public var parentView: UIView?
     public var contentView = UIScrollView()
+    private var anchorPosition: AnchorPosition = .closed
+    public var animationSpeed: Double = 0.2
+    
     private func configureView() {
         guard let parentView = self.parentView else { return }
         
@@ -51,21 +54,24 @@ public class AnchorView: UIView {
 }
     private func configureView() {
         guard let parentView = self.parentView else { return }
+
+extension AnchorView {
+    private func animateTo(_ position: AnchorPosition, completion: (() -> Void)? = nil) {
+        anchorPosition = position
         
-        parentView.addSubview(self)
-        translatesAutoresizingMaskIntoConstraints = false
-        leadingAnchor.constraint(equalTo: parentView.leadingAnchor).isActive = true
-        trailingAnchor.constraint(equalTo: parentView.trailingAnchor).isActive = true
-        topAnchor.constraint(equalTo: parentView.topAnchor).isActive = true
-        bottomAnchor.constraint(equalTo: parentView.bottomAnchor).isActive = true
+        layoutIfNeeded()
         
-        addSubview(contentView)
-        contentView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        contentView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-        contentView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-        heightContraint.isActive = true
+        let height = (parentView?.frame.height ?? 0) * position.rawValue
+        self.heightContraint.constant = height
+        UIView.animate(withDuration: animationSpeed, animations: {
+            self.layoutIfNeeded()
+        }, completion: { _ in
+            completion?()
+        })
     }
+    }
+}
+
 extension AnchorView: UIGestureRecognizerDelegate {
     public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         return true
